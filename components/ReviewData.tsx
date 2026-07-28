72798a970f60fcf4925f5163b898ae5876dfaa46
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-29 03:24:28 +0500

    review panel done

--- components/ReviewData.tsx
+++ components/ReviewData.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { CartData } from '@/type';
+import { CartData, ProductCart } from '@/type';
 import React, { useEffect, useState } from 'react'
 
 const ReviewData = () => {
-  const [data, setData] = useState<CartData | null>(null)
+  const [data, setData] = useState<CartData[] | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,10 +25,65 @@ const ReviewData = () => {
     }
     
   }, [])
-  console.log(data)
   return (
     <div>
-      {}
+      {data?.map((cart, index) => (
+      <div key={index}>
+
+        {Object.entries(cart).map(([category, items]) => {
+
+          if (category === "totals") return null;
+
+          return (
+            <div className="border-t mt-4 border-review-border pt-[14px] flex flex-col gap-[12px]" key={category}>
+              <h3 className="text-sm text-review-heading uppercase tracking-wider">
+                {category}
+              </h3>
+
+              <div className="flex flex-col gap-[12px] ">
+                {(items as ProductCart[]).map((item) =>  {
+                  const productName = item.name === "Cam Unlimited" ? item.name.split(' ') : item.name
+
+                  return  (
+                  <div className="flex gap-[16px] items-center" key={item.productId}>
+                    <div className="flex flex-2 justify-between items-center">
+                      <div className="flex flex-row items-center gap-[12px]">
+                        <img className="w-[41px] h-[41px] rounded-[5px]" src={item.image} alt={item.name} />
+                        <h3 className={`${item.name === "Cam Unlimited" ? "font-semibold text-[16px]" : "font-medium text-sm"}`}>
+                          {item.name === "Cam Unlimited" ? (
+                            <>
+                              {productName[0]}{" "}
+                              <span className="text-purple">{productName[1]}</span>
+                            </>
+                          ) : (
+                            item.name
+                          )}
+                          {item.variantLabel && ` - ${item.variantLabel}`}
+                        </h3>
+                      </div>
+                      {!item.billingPeriod && <div className="flex justify-between py-[4px] w-[77px]">
+                        {/* quantity */}
+                        <button className={`${item.requiredItem && !item.editable ? "bg-[#F1F1F2] border border-[#CED6DE]" : "bg-white "} flex items-center justify-center w-[22px] h-[22px] rounded-[4px] font-medium`}>-</button>
+                        <p className="font-semibold text-sm">{item.quantity}</p>
+                        <button className={`${item.requiredItem && !item.editable ? "bg-[#F1F1F2] border border-[#CED6DE]" : "bg-white "} flex items-center justify-center w-[22px] h-[22px] rounded-[4px] font-medium`}>+</button>
+                      </div>}
+                    </div>
+                    <div>
+                      {item.compareAtPrice && <p className="font-medium text-sm text-review-cut-price line-through text-right">${item.compareAtPrice}{item.billingPeriod && <span>/{item.billingPeriod}</span>}</p>}
+                      {item.price === 0 ? <p className="font-semibold text-sm text-purple text-right">FREE</p> : <p className="font-semibold text-right text-sm text-purple">${item.price}{item.billingPeriod && <span>/{item.billingPeriod}</span>}</p>}
+                    </div>
+                  </div>
+                  )
+                }
+                )}
+              </div>
+
+            </div>
+          );
+        })}
+
+      </div>
+    ))}
     </div>
   )
 }
--- sections/ReviewPanel.tsx
+++ sections/ReviewPanel.tsx
@@ -11,7 +11,7 @@ const ReviewPanel = () => {
           REVIEW
         </h3>
       </div>
-      <div className="flex flex-col gap-[12px ] px-[20px] pt-[20px] pb-[31px]">
+      <div className="flex flex-col gap-[12px] px-[20px] pt-[20px] pb-[31px]">
         <div className="">
           <h4 className="font-medium text-[22px]">Your security system</h4>
           <p className="text-black/75 text-[15px] mt-1">Review your personalized protection system designed to keep what matters most safe</p>
@@ -21,14 +21,14 @@ const ReviewPanel = () => {
           
           <ReviewData />
 
-          <div className="border-t border-review-border flex flex-row justify-between items-center pt-[15px]">
+          <div className="border-t mt-4 border-review-border flex flex-row justify-between items-center pt-[14px]">
             <div className='flex items-center gap-[13px]'>
-              <FaTruckFast size={32} className=" bg-white p-[2px] rounded-[5px] text-green" />
+              <FaTruckFast size={32} className="w-[41px] h-[41px] bg-white p-[2px] rounded-[5px] text-green" />
               <p className="text-sm font-medium">Fast Shipping</p>
             </div>
             <div className="flex flex-col">
               <p className="font-medium text-review-cut-price line-through text-sm text-right">$5.99</p>
-              <p className="text-purple font-bold text-sm">FREE</p>
+              <p className="text-purple text-right font-bold text-sm">FREE</p>
             </div>
           </div>
 
@@ -48,7 +48,7 @@ const ReviewPanel = () => {
                   </div>
                 </div>
               </div>
-              <div className="pt-2.5">
+              <div className="pt-3">
                 <p className="text-green mb-1 text-center text-sm font-medium">Congrats! You&apos;re saving $50.92 on your security bundle!</p>
                 <button className="bg-purple text-white text-[19px] font-bold tracking-wide text-center rounded-[5px] w-full py-3.25 px-4">Checkout</button>
               </div>
