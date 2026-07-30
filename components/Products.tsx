c8f57ca85e787b727e0cbf35c666fdab6162544c
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-30 18:44:09 +0500

    trying new layout

--- components/Products.tsx
+++ components/Products.tsx
@@ -17,20 +17,20 @@ const Products = ({product, length, index}: Props) => {
 
   return (
     <div className={`p-[11px] bg-white w-full 2xl:w-[381px] items-center h-full flex flex-col 2xl:flex-row gap-[13px] rounded-[10px] ${isLast && isOdd ? "2xl:col-span-2 2xl:flex 2xl:justify-center" : ""}`}>
-      <div className="relative w-full 2xl:w-[160px] h-[180px] ">
+      <div className="relative w-full 2xl:w-[160px] h-[170px] ">
         {/* badge */}
         {product.badge && <span className="absolute top-2 left-2 bg-purple text-white py-[2px] px-[6px] rounded-[10px] font-semibold text-xs z-20 ">{product.badge}</span>}
         <Image src={product.image} alt={product.name} fill className="w-full h-full object-contain" />
       </div>
-      <div className="flex flex-col gap-[13px] w-full">
+      <div className="flex flex-col gap-[12px] w-full">
         <div className="flex flex-col gap-[10px]">
-            <h2 className="font-semibold text-[17px] tracking-[0.6px]">{product.name}</h2>
-            <p className="font-medium text-sm text-black/80">{product.description} {product.learnMoreUrl !== undefined && <Link target="_blank" className="text-purple underline" href={product.learnMoreUrl}>Learn More</Link>}</p>
+            <h2 className="font-semibold text-[16px] tracking-[0.6px]">{product.name}</h2>
+            <p className="font-medium text-[13px] text-black/80">{product.description} {product.learnMoreUrl !== undefined && <Link target="_blank" className="text-purple underline" href={product.learnMoreUrl}>Learn More</Link>}</p>
            {product.variants && <div className="flex gap-[6px]">
                 {product.variants.map(v => (
                     <div className="rounded-[2px] border-[0.5] py-[1px] px-[5px] bg-white/4 border-variant-selector-border cursor-pointer flex items-center" key={v.variantId}>
                         <Image src={v.variantImage} alt={v.label} width={100} height={100} className="w-6 h-6 object-contain" />
-                        <p className="font-medium text-xs">{v.label}</p>
+                        <p className="font-medium text-[10px]">{v.label}</p>
                     </div>
                 ))}
             </div>}
